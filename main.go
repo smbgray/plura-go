68c15756b24e0a24bbe352994bc08829c264b9af
Oleksandr.Bilochenko <oleksandr.bilochenko@ing.com>
2022-04-20 13:53:54 +0200

    update

--- main.go
+++ main.go
@@ -11,9 +11,16 @@ func main() {
 
 	fmt.Println(f)
 
-	firstName := "Alex"
+	firstName := "Alex2"
 	fmt.Println(firstName)
 	c := complex(3, 4)
 	r, im := real(c), imag(c)
 	fmt.Println(r, im)
+	// pointers
+
+	name := "Alex"
+	ptr := &name
+	ptr1 := &firstName
+	*ptr = *ptr1
+	fmt.Println(ptr, *ptr)
 }
