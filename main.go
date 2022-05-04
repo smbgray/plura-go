50eb5e529815b9856c5d2f0b6c285a5855d9b6a4
Oleksandr.Bilochenko <oleksandr.bilochenko@ing.com>
2022-05-04 13:30:26 +0200

    updates

--- main.go
+++ main.go
@@ -1,26 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/smbgray/plura-go/models"
+)
 
 func main() {
-	var i int
-	i = 42
-	fmt.Println(i)
+	u := models.User{
+		ID:        2,
+		FirstName: "Alex",
+		LastName:  "Biii",
+	}
+	fmt.Println(u)
+}
 
-	var f float32 = 3.141
-
-	fmt.Println(f)
-
-	firstName := "Alex2"
-	fmt.Println(firstName)
-	c := complex(3, 4)
-	r, im := real(c), imag(c)
-	fmt.Println(r, im)
-	// pointers
-
-	name := "Alex"
-	ptr := &name
-	ptr1 := &firstName
-	*ptr = *ptr1
-	fmt.Println(ptr, *ptr)
+func stratWebServer() {
+	fmt.Println("Starting server....")
+	//do smth
+	fmt.Println("Server started")
 }
