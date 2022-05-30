22d3ddcf18d68cb9b4a1456cae7ca029470d23b9
smbgray <smbgray@gmail.com>
2022-05-30 21:38:17 +0200

    more testing added

--- messages/messages_test.go
+++ messages/messages_test.go
@@ -19,3 +19,21 @@ func TestDepart(t *testing.T) {
 		t.Errorf("did not get expected result. Wanted %q, got %q\n", expect, got)
 	}
 }
+
+func TestGreetTableDriven(t *testing.T) {
+	scenarios := []struct {
+		input  string
+		expect string
+	}{
+		{input: "Gopher", expect: "Hello, Gopher!\n"},
+		{input: "", expect: "Hello, !\n"},
+	}
+	for _, s := range scenarios {
+		got := Greet(s.input)
+		if got != s.expect {
+			t.Errorf("Did not get expected result for input '%v'. Expected %q, got %q",
+				s.input, got, s.expect)
+		}
+	}
+
+}
